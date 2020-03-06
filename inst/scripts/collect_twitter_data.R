99d2f6acf03f483f38ec215c50e88211d87d4f6f
Malcolm Barrett <malcolmbarrett@gmail.com>
2020-03-06 08:53:16 -0500

    fix email list

--- inst/scripts/collect_twitter_data.R
+++ inst/scripts/collect_twitter_data.R
@@ -3,6 +3,6 @@ library(rtweet)
 options(gargle_oob_default = TRUE)
 options(gargle_oauth_email = gmail("ser.twitteracct"))
 
-on_error_email_to(gmail("malcolmbarrett"))
+on_error_email_to(c(gmail("malcolmbarrett"), email("jason_gantenberg", "brown.edu")))
 safely_collect_twitter_data <- action_safely(action_collect_twitter_data)
 safely_collect_twitter_data()
--- inst/scripts/deploy_dashboard.R
+++ inst/scripts/deploy_dashboard.R
@@ -3,6 +3,6 @@ library(rtweet)
 options(gargle_oob_default = TRUE)
 options(gargle_oauth_email = gmail("ser.twitteracct"))
 
-on_error_email_to(gmail("malcolmbarrett"))
+on_error_email_to(c(gmail("malcolmbarrett"), email("jason_gantenberg", "brown.edu")))
 safely_deploy_dashboard <- action_safely(action_deploy_dashboard)
 safely_deploy_dashboard()
