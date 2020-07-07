e68a838771a9126abdb76057fc90eb99ef322b69
Malcolm Barrett <malcolmbarrett@gmail.com>
2020-07-07 07:48:35 -0700

    update email list

--- inst/scripts/email_summary.R
+++ inst/scripts/email_summary.R
@@ -4,7 +4,6 @@ on_error_email_to(c(gmail("malcolmbarrett"), email("jason_gantenberg", "brown.ed
 safe_action_email_summary <- action_safely(action_email_summary, "(email_summary.R)")
 send_summary_to <- c(
   gmail("malcolmbarrett"),
-  email("Anusha.Vable", "ucsf.edu"),
   email("sbevan", "epiresearch.org"),
   email("mumfords", "mail.nih.gov"),
   email("Magdalena.Cerda", "nyulangone.org"),
