1e09621857c71a68112175ec1d5dd71ec32b4546
jrgant <jason.gantenberg@gmail.com>
2021-12-10 19:08:06 -0500

    add and export sandbox_token()

--- R/tokens.R
+++ R/tokens.R
@@ -20,6 +20,18 @@ ser_token <- function() {
   )
 }
 
+#' @export
+#' @rdname drive_auth_token
+sandbox_token <- function() {
+  rtweet::create_token(
+    "twitter_sandbox_test",
+    consumer_key = Sys.getenv("SANDBOX_CONSUMER_KEY"),
+    consumer_secret = Sys.getenv("SANDBOX_CONSUMER_SECRET"),
+    access_token = Sys.getenv("SANDBOX_ACCESS_TOKEN"),
+    access_secret = Sys.getenv("SANDBOX_ACCESS_SECRET"),
+    set_renv = FALSE
+  )
+}
 
 #' @export
 #' @rdname drive_auth_token
