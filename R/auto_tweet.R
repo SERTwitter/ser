be589cbc36e166533dfed0adb8ebda2c7ffb14dd
jrgant <jason.gantenberg@gmail.com>
2021-12-10 19:08:37 -0500

    update tweet library processing

--- R/auto_tweet.R
+++ R/auto_tweet.R
@@ -1,16 +1,34 @@
 # functions to post tweet depending on type
-post_tweet_queue <- function(tweet_data = tweet_queue, twitter_token = ser_token) {
-  tweet <- tweet_data %>%
-    dplyr::filter(!tweeted) %>%
-    dplyr::sample_n(1) %>%
-    dplyr::pull(text)
-
+post_tweet_library <- function(tweet_data = tweet_library,
+                               past_tweets = tweet_hist_ids,
+                               twitter_token = ser_token) {
   tweet_data <- tweet_data %>%
-    dplyr::mutate(tweeted = ifelse(text == tweet, TRUE, tweeted))
+    dplyr::mutate(
+      id_transform =
+        stringr::str_remove_all(Timestamp, pattern = "\\s|:|/")
+    )
 
-  rtweet::post_tweet(tweet, token = twitter_token())
+  id_check <- tweet_data %>%
+    dplyr::filter(!id_transform %in% past_tweets)
 
-  invisible(tweet_data)
+  if (nrow(id_check) == 0) {
+    tweet_data <- tweet_data %>%
+      dplyr::sample_n(1)
+    restart_history <- TRUE
+  } else {
+    tweet_data <- id_check %>%
+      dplyr::sample_n(1)
+    restart_history <- FALSE
+  }
+
+  rtweet::post_tweet(tweet_data$Tweet, token = twitter_token)
+
+  invisible(
+    list(
+      restart_history = restart_history,
+      just_tweeted = tweet_data$id_transform
+    )
+  )
 }
 
 update_retweets <- function(twitter_token = ser_token) {
@@ -43,6 +61,11 @@ blackout_tweet <- function(tweet_data = tweet_queue) {
   invisible(tweet_data)
 }
 
+get_day <- function() {
+  lubridate::wday(Sys.Date(), label = TRUE) %>%
+    as.character()
+}
+
 #' Post SER tweets from the tweet queue Monday through Friday
 #'
 #' `action_auto_tweet()` handles daily tweeting on the SER account. It randomly pulls
@@ -59,47 +82,74 @@ blackout_tweet <- function(tweet_data = tweet_queue) {
 #' @return a data frame containing the updated tweet queue
 #'
 #' @export
-action_auto_tweet <- function(twitter_token = ser_token, google_drive_auth = drive_auth_token()) {
+action_auto_tweet <- function(twitter_token = ser_token,
+                              google_drive_auth = drive_auth_token(),
+                              todays_date = get_day()) {
+
   # Authorize Google Drive for cron job
   # previous code
   # ttt <- googledrive::drive_auth(google_drive_auth)
   # if (!is.null(google_drive_auth)) saveRDS(ttt, google_drive_auth)
   googledrive::drive_auth(path = google_drive_auth)
 
-  # download the existing tweet queue from google drive
-  tweet_csv_id <- googledrive::drive_find(pattern = "^tweet_queue", type = "spreadsheet") %>%
+  # download the existing tweet form entries from google drive
+  tweet_library_id <- googledrive::drive_find(
+    pattern = "ser_tweet_library",
+    type = "spreadsheet"
+  ) %>%
     dplyr::pull(id) %>%
     googledrive::as_id()
-  googledrive::drive_download(tweet_csv_id, type = "csv", overwrite = TRUE)
-  tweet_queue <- readr::read_csv("tweet_queue.csv")
+  googledrive::drive_download(tweet_library_id, type = "csv", overwrite = TRUE)
+  tweet_library <- readr::read_csv("ser_tweet_library.csv")
+
+  # download tweet history
+  tweet_hist_id <- googledrive::drive_find(
+    pattern = "ser_tweet_history",
+    type = "spreadsheet"
+  ) %>%
+    dplyr::pull(id) %>%
+    googledrive::as_id()
+  googledrive::drive_download(tweet_hist_id, type = "csv", overwrite = TRUE)
+  tweet_hist <- readr::read_csv("ser_tweet_history.csv", col_types = "c")
+  tweet_hist_ids <- tweet_hist$tweet_id
 
   # do the same for the retweets queue
-  retweet_csv_id <- googledrive::drive_find(pattern = "retweet_queue", type = "spreadsheet") %>%
+  retweet_csv_id <- googledrive::drive_find(
+    pattern = "retweet_queue",
+    type = "spreadsheet"
+  ) %>%
     dplyr::pull(id) %>%
     googledrive::as_id()
   googledrive::drive_download(retweet_csv_id, type = "csv", overwrite = TRUE)
   retweet_queue <- readr::read_csv("retweet_queue.csv", col_types = "cciil")
 
-  # don't post on weekends
-  todays_date <- lubridate::wday(Sys.Date(), label = TRUE) %>%
-    as.character()
+
+  # check date and choose tweet type
   if (todays_date %in% c("Mon", "Tue", "Wed", "Thu", "Fri")) {
-    post_tweet_of_type <- post_tweet_queue
+    post_tweet_of_type <- post_tweet_library
   } else {
     post_tweet_of_type <- blackout_tweet
   }
 
   # 1/10 days, retweet a popular SER tweet instead of posting a new one
   retweet_day <- sample(c(TRUE, FALSE), 1, prob = c(.1, .9))
-  if (retweet_day) post_tweet_of_type <- post_retweet
+  if (retweet_day) post_tweet_of_type <- post_retweet(twitter_token = twitter_token)
 
   # check that it's not a blackout date
   # if it is, don't post anything
-  blackout_id <- googledrive::drive_find(pattern = "tweet_blackout", type = "spreadsheet") %>%
+  blackout_id <- googledrive::drive_find(
+    pattern = "tweet_blackout",
+    type = "spreadsheet"
+  ) %>%
     dplyr::pull(id) %>%
     googledrive::as_id()
+
   googledrive::drive_download(blackout_id, type = "csv", overwrite = TRUE)
-  tweet_blackout <- readr::read_csv("tweet_blackout.csv", col_types = list(date = readr::col_date("%m/%d/%Y")))
+  tweet_blackout <- readr::read_csv(
+    "tweet_blackout.csv",
+    col_types = list(date = readr::col_date("%m/%d/%Y"))
+  )
+
   blackout <- lubridate::today() %in% tweet_blackout$date
   if (blackout) post_tweet_of_type <- blackout_tweet
 
@@ -107,17 +157,24 @@ action_auto_tweet <- function(twitter_token = ser_token, google_drive_auth = dri
   if (retweet_day) {
     retweet_queue <- post_tweet_of_type(retweet_queue)
   } else {
-    tweet_queue <- post_tweet_of_type(tweet_queue)
+    tweet_library_status <- post_tweet_of_type(tweet_library, tweet_hist_ids, twitter_token)
   }
 
-  if (all(tweet_queue$tweeted, na.rm = TRUE)) tweet_queue$tweeted <- FALSE
+  if (tweet_library_status$restart_history) {
+    tweet_hist <- dplyr::tibble(tweet_id = tweet_library_status$just_tweeted)
+  } else {
+    tweet_hist <- dplyr::tibble(
+      tweet_id = c(tweet_hist_ids, tweet_library_status$just_tweeted)
+    )
+  }
+
 
   # update tweet and retweet queues
-  readr::write_csv(tweet_queue, "tweet_queue.csv")
-  googledrive::drive_update(tweet_csv_id, "tweet_queue.csv")
+  readr::write_csv(tweet_hist, "ser_tweet_history.csv")
+  googledrive::drive_update(tweet_hist_id, "ser_tweet_history.csv")
   readr::write_csv(retweet_queue, "retweet_queue.csv")
   googledrive::drive_update(retweet_csv_id, "retweet_queue.csv")
 
   # return updated tweet queue
-  invisible(tweet_queue)
+  invisible(tweet_hist)
 }
